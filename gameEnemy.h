f0962beffad98b1d1be5ca40cb4a982518656b46
ngschmidt <nick.schmidt3@gmail.com>
2013-04-22 13:05:56 -0700

    Create gameEnemy.h
    initial file for class gameEnemy header

---
+++ gameEnemy.h
@@ -0,0 +1,29 @@
+/*
+*
+*
+*		Example Enemy Class Header
+*		Nick Schmidt
+*		22 Apr 2013
+*
+*
+*/
+
+
+
+class gameEnemy {
+
+//default ctor, sets to the default values below if no other params have been passed
+gameEnemy();
+
+//primary ctor, assigns to enemyName, enemyHealth, enemyMaxHealth variables
+gameEnemy(String name1, int health1, int maxHealth1);
+
+public:
+
+protected:
+
+private:
+	string enemyName;
+	int enemyHealth, enemyMaxHealth;
+
+};
