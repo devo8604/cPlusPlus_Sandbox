dadcc6c484fbb8ff1aaf91c26ec0e4022c7aa125
ngschmidt <nick.schmidt3@gmail.com>
2013-04-22 13:09:51 -0700

    Create gameEnemy.cpp
    initial commit

---
+++ gameEnemy.cpp
@@ -0,0 +1,15 @@
+/*
+*
+*
+* Example Enemy Class Functions
+* Nick Schmidt
+* 22 Apr 2013
+*
+*
+*/
+
+void gameEnemy::gameEnemyenemy(String name1, int health1, int maxHealth1) {
+	enemyName = name1;
+	enemyHealth = health1;
+	enemyMaxHealth = maxHealth1;
+}
